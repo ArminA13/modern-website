741292dd171fa65562acebb25f541fa00bcf9790
ArmineMuradyan <muradyan.armin13@gmail.com>
2024-02-21 23:17:21 +0400

    Add About Hero components

---
+++ sections/Explore.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from '../styles';
+import {fadeIn, staggerContainer} from '../utils/motion'
+
+const Explore = () => (
+  <section>
+    
+  </section>
+);
+
+export default Explore;
